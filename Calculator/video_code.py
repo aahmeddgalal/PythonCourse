71688ae891a99d24d7bfeb1637f1ab73ab17eda1
Galileo <aahmeddgalal@gmail.com>
2026-06-05 18:07:30 +0300

    finish first video

---
+++ Calculator/video_code.py
@@ -0,0 +1,16 @@
+# Basic Calculator
+# print , input , Variables, Basic Arithmetic Operations
+
+
+num1 = float(input("First Number: "))
+num2 = float(input("Second Number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
